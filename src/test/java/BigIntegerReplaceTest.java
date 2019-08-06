fd6d639f6e7f143d8e6a66b66dafb565cc812cab
Arkady Sapozhnikov <arksap2002@gmail.com>
2019-08-06 18:06:07 +0300

    Commit

--- src/test/java/BigIntegerReplaceTest.java
+++ src/test/java/BigIntegerReplaceTest.java
@@ -1,5 +1,3 @@
-import com.github.javaparser.JavaParser;
-import com.github.javaparser.ast.CompilationUnit;
 import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 
@@ -11,15 +9,11 @@ import static org.junit.Assert.*;
 public class BigIntegerReplaceTest {
 
 
-    public void runTestOnFile(final String filename, final Object object) throws IOException {
-        assertEquals(IOUtils.resourceToString(filename, Charset.defaultCharset()), object);
-    }
-
     @Test
     public void transform() throws IOException {
         String filename = "/Foo.before.java";
         Charset charset = Charset.defaultCharset();
         String string = IOUtils.resourceToString(filename, charset);
-        runTestOnFile("/Foo.after.java", string);
+        assertEquals(IOUtils.resourceToString("/Foo.after.java", Charset.defaultCharset()), string);
     }
 }
