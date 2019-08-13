6d2febf5b2a418133a0282c4e2ab1706e53165b1
Arkady Sapozhnikov <arksap2002@gmail.com>
2019-08-14 02:00:17 +0300

    Commit

--- src/test/resources/NothingChanges.before.java
+++ src/test/resources/NothingChanges.before.java
@@ -1,16 +1,6 @@
 public class NothingChanges {
 
-    int a = 20;
-    long b = 200;
-    String string = "Two";
-    double c = 2.2;
-    java.math.BigInteger bigInteger = BigInteger.TWO;
-
     public static void main(String[] args) {
-        int a = 10;
-        long b = 100;
-        String string = "One";
-        double c = 1.1;
-        java.math.BigInteger bigInteger = BigInteger.ONE;
+        System.out.println("Hello, world!");
     }
 }
