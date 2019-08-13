2a3836420c514f546668b8169cf9fd3dd6738025
Arkady Sapozhnikov <arksap2002@gmail.com>
2019-08-13 16:58:04 +0300

    Commit

--- src/main/java/BigIntegerReplace.java
+++ src/main/java/BigIntegerReplace.java
@@ -1,5 +1,6 @@
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.VariableDeclarator;
 import com.github.javaparser.ast.expr.MethodCallExpr;
 import com.github.javaparser.ast.expr.SimpleName;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
@@ -7,9 +8,14 @@ import com.github.javaparser.symbolsolver.JavaSymbolSolver;
 import com.github.javaparser.symbolsolver.javaparsermodel.JavaParserFacade;
 import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver;
 
+import java.util.ArrayList;
+
+
 import static com.github.javaparser.ast.Node.SYMBOL_RESOLVER_KEY;
 
 public final class BigIntegerReplace {
+
+    public static ArrayList<String> scannerList = new ArrayList<>();
 
     public String transform(final String string) {
         CompilationUnit compilationUnit = JavaParser.parse(string);
@@ -31,10 +37,25 @@ public final class BigIntegerReplace {
                 final MethodCallExpr n,
                 final JavaParserFacade javaParserFacade) {
             super.visit(n, javaParserFacade);
-            if ((n.getName().getIdentifier().equals("nextInt"))) {
-                SimpleName simpleName = new SimpleName();
-                simpleName.setIdentifier("nextBigInteger");
-                n.setName(simpleName);
+            for (String scannerName : scannerList) {
+                if ((n.getName().getIdentifier().equals("nextInt")) &&
+                        (n.getScope().get().asNameExpr().getName().toString().
+                                equals(scannerName))) {
+                    SimpleName simpleName = new SimpleName();
+                    simpleName.setIdentifier("nextBigInteger");
+                    n.setName(simpleName);
+                }
+            }
+        }
+
+        @Override
+        public void visit(VariableDeclarator n,
+                          JavaParserFacade javaParserFacade) {
+            super.visit(n, javaParserFacade);
+            if (n.getType().isClassOrInterfaceType() &&
+                    n.getType().asClassOrInterfaceType().getName().toString().
+                            equals("Scanner")) {
+                scannerList.add(n.getName().toString());
             }
         }
     }
