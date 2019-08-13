a715d54ee8739293f211221da667b552304d703b
Arkady Sapozhnikov <arksap2002@gmail.com>
2019-08-14 00:20:22 +0300

    Commit

--- src/main/java/BigIntegerReplace.java
+++ src/main/java/BigIntegerReplace.java
@@ -1,12 +1,38 @@
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.VariableDeclarator;
+import com.github.javaparser.ast.type.ClassOrInterfaceType;
+import com.github.javaparser.ast.type.PrimitiveType;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+import com.github.javaparser.symbolsolver.JavaSymbolSolver;
+import com.github.javaparser.symbolsolver.javaparsermodel.JavaParserFacade;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver;
 
-import java.io.IOException;
+import static com.github.javaparser.ast.Node.SYMBOL_RESOLVER_KEY;
 
 public final class BigIntegerReplace {
 
-    public String transform(final String string) throws IOException {
-        CompilationUnit cu = JavaParser.parse(string);
-        return cu.toString();
+    public String transform(final String string) {
+        CompilationUnit compilationUnit = JavaParser.parse(string);
+        ReflectionTypeSolver reflectionTypeSolver =
+                new ReflectionTypeSolver();
+        compilationUnit.setData(
+                SYMBOL_RESOLVER_KEY,
+                new JavaSymbolSolver(reflectionTypeSolver));
+        compilationUnit.accept(
+                new TransformVisitor(),
+                JavaParserFacade.get(reflectionTypeSolver));
+        return compilationUnit.toString();
+    }
+
+    static class TransformVisitor
+            extends VoidVisitorAdapter<JavaParserFacade> {
+        @Override
+        public void visit(
+                final VariableDeclarator n,
+                final JavaParserFacade javaParserFacade) {
+            super.visit(n, javaParserFacade);
+
+        }
     }
 }
