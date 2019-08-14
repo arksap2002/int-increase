e2b47757c45f582f473784a177194e54aab18a84
Arkady Sapozhnikov <arksap2002@gmail.com>
2019-08-14 22:34:43 +0300

    Commit

--- src/main/java/BigIntegerReplace.java
+++ src/main/java/BigIntegerReplace.java
@@ -6,7 +6,6 @@ import com.github.javaparser.ast.expr.FieldAccessExpr;
 import com.github.javaparser.ast.expr.IntegerLiteralExpr;
 import com.github.javaparser.ast.expr.MethodCallExpr;
 import com.github.javaparser.ast.expr.NameExpr;
-import com.github.javaparser.ast.body.VariableDeclarator;
 import com.github.javaparser.ast.type.ClassOrInterfaceType;
 import com.github.javaparser.ast.type.PrimitiveType;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
