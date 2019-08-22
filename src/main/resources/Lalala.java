caa26d9ccc61a9cea2dfb482563c315450df4905
Arkady Sapozhnikov <arksap2002@gmail.com>
2019-08-22 22:24:59 +0300

    Commit

--- src/main/resources/Lalala.java
+++
@@ -1,9 +0,0 @@
-import java.math.BigInteger;
-
-public class NothingChanges {
-
-    public static void main(String[] args) {
-        BigInteger bigInteger = BigInteger.ONE.negate();
-        BigInteger bigInteger1 = BigInteger.valueOf(100).negate();
-    }
-}
