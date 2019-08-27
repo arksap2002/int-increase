edad91abb71b258d942a6a08ebb68fbdeed1f2d3
Arkady Sapozhnikov <arksap2002@gmail.com>
2019-08-28 01:20:57 +0300

    Commit

--- src/main/resources/input.java
+++
@@ -1,7 +0,0 @@
-public class NothingChanges {
-
-    public static void main(String[] args) {
-        java.math.BigInteger bigInteger = java.math.BigInteger.ONE;
-        java.math.BigInteger bigInteger2 = java.math.BigInteger.valueOf(100);
-    }
-}
