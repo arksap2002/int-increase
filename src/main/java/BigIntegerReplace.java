8e2722c95d9befab84af89fbbc07e648479a4649
Arkady Sapozhnikov <arksap2002@gmail.com>
2019-08-31 13:14:20 +0300

    Commit

--- src/main/java/BigIntegerReplace.java
+++ src/main/java/BigIntegerReplace.java
@@ -2,7 +2,6 @@ import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.NodeList;
 import com.github.javaparser.ast.body.VariableDeclarator;
-import com.github.javaparser.ast.expr.SimpleName;
 import com.github.javaparser.ast.expr.Expression;
 import com.github.javaparser.ast.expr.MethodCallExpr;
 import com.github.javaparser.ast.expr.FieldAccessExpr;
@@ -11,7 +10,6 @@ import com.github.javaparser.ast.expr.IntegerLiteralExpr;
 import com.github.javaparser.ast.type.ClassOrInterfaceType;
 import com.github.javaparser.ast.type.PrimitiveType;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
-import com.github.javaparser.resolution.declarations.ResolvedMethodDeclaration;
 import com.github.javaparser.symbolsolver.JavaSymbolSolver;
 import com.github.javaparser.symbolsolver.javaparsermodel.JavaParserFacade;
 import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver;
@@ -35,21 +33,6 @@ public final class BigIntegerReplace {
 
     class TransformVisitor
             extends VoidVisitorAdapter<JavaParserFacade> {
-        @Override
-        public void visit(
-                final MethodCallExpr n,
-                final JavaParserFacade javaParserFacade) {
-            super.visit(n, javaParserFacade);
-            ResolvedMethodDeclaration resolvedMethodDeclaration = n.resolve();
-            if ((resolvedMethodDeclaration.getName().equals("nextInt"))) {
-                if (resolvedMethodDeclaration.getPackageName().
-                        equals("java.util") && resolvedMethodDeclaration.
-                        getClassName().equals("Scanner")) {
-                    n.setName(new SimpleName("nextBigInteger"));
-                }
-            }
-        }
-
         @Override
         public void visit(
                 final VariableDeclarator n,
