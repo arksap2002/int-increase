25f7152ce8bafb7ef431d9756479109ac77d7cc9
Arkady <arksap2002@gmail.com>
2019-09-06 23:20:47 +0300

    Commit

--- src/test/java/BigIntegerReplaceTest.java
+++ src/test/java/BigIntegerReplaceTest.java
@@ -9,43 +9,48 @@ import static org.junit.Assert.*;
 
 public class BigIntegerReplaceTest {
 
-    private void runTestFromFile(String filePrefix) throws IOException, IllegalAccessException, InstantiationException, ClassNotFoundException {
+    private void runTestFromFile(String filePrefix) throws IOException, ClassNotFoundException {
         BigIntegerReplace bigIntegerReplace = new BigIntegerReplace();
-        ((Runnable) (CompilerUtils.CACHED_COMPILER.loadFromJava("src.test.resources." + filePrefix + ".before.java",
-                IOUtils.resourceToString("/" + filePrefix + ".before.java", Charset.defaultCharset()))).newInstance()).run();
-        ((Runnable) (CompilerUtils.CACHED_COMPILER.loadFromJava("src.test.resources." + filePrefix + ".after.java",
-                IOUtils.resourceToString("/" + filePrefix + ".after.java", Charset.defaultCharset()))).newInstance()).run();
-        String gotOutput = bigIntegerReplace.transform(IOUtils.resourceToString("/" + filePrefix + ".before.java", Charset.defaultCharset()));
-        assertEquals(IOUtils.resourceToString("/" + filePrefix + ".after.java", Charset.defaultCharset()), gotOutput);
+        CompilerUtils.CACHED_COMPILER.loadFromJava(filePrefix,
+                IOUtils.resourceToString("/" +
+                        filePrefix + ".before.java", Charset.defaultCharset()));
+        CompilerUtils.CACHED_COMPILER.loadFromJava(filePrefix,
+                IOUtils.resourceToString("/" +
+                        filePrefix + ".after.java", Charset.defaultCharset()));
+        String gotOutput = bigIntegerReplace.transform(
+                IOUtils.resourceToString("/" +
+                        filePrefix + ".before.java", Charset.defaultCharset()));
+        assertEquals(IOUtils.resourceToString("/" +
+                filePrefix + ".after.java", Charset.defaultCharset()), gotOutput);
     }
 
     @Test
-    public void testNothingChanges() throws IOException, IllegalAccessException, ClassNotFoundException, InstantiationException {
+    public void testNothingChanges() throws IOException, ClassNotFoundException {
         runTestFromFile("NothingChanges");
     }
 
     @Test
-    public void testReplaceVariableDeclarationType() throws IOException, IllegalAccessException, ClassNotFoundException, InstantiationException {
+    public void testReplaceVariableDeclarationType() throws IOException, ClassNotFoundException {
         runTestFromFile("ReplaceVariableDeclarationType");
     }
 
     @Test
-    public void testArithmeticOperations() throws IOException, IllegalAccessException, ClassNotFoundException, InstantiationException {
+    public void testArithmeticOperations() throws IOException, ClassNotFoundException {
         runTestFromFile("ArithmeticOperations");
     }
 
     @Test
-    public void testUnary() throws IOException, IllegalAccessException, ClassNotFoundException, InstantiationException {
+    public void testUnary() throws IOException, ClassNotFoundException {
         runTestFromFile("Unary");
     }
   
     @Test
-    public void testScannerFromImport() throws IOException, IllegalAccessException, ClassNotFoundException, InstantiationException {
+    public void testScannerFromImport() throws IOException, ClassNotFoundException {
         runTestFromFile("ScannerFromImport");
     }
 
     @Test
-    public void testScannerWithClass() throws IOException, IllegalAccessException, ClassNotFoundException, InstantiationException {
+    public void testScannerWithClass() throws IOException, ClassNotFoundException {
         runTestFromFile("ScannerWithClass");
     }
 }
