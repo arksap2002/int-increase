649a8a92d03fa7fa75d53b56a79eb64af85ed62e
Arkady <arksap2002@gmail.com>
2019-09-08 11:51:03 +0300

    Commit

--- src/test/resources/Unary.after.java
+++ src/test/resources/Unary.after.java
@@ -1,23 +1,23 @@
 public class Unary {
 
-    java.math.BigInteger a = java.math.BigInteger.TWO.negative();
+    java.math.BigInteger a = java.math.BigInteger.TWO.negate();
 
     java.math.BigInteger b = java.math.BigInteger.valueOf(3);
 
-    java.math.BigInteger c = java.math.BigInteger.TWO.negative().add(java.math.BigInteger.valueOf(3));
+    java.math.BigInteger c = java.math.BigInteger.TWO.negate().add(java.math.BigInteger.valueOf(3));
 
-    java.math.BigInteger d = java.math.BigInteger.valueOf(3).subtract(java.math.BigInteger.TWO.negative());
+    java.math.BigInteger d = java.math.BigInteger.valueOf(3).subtract(java.math.BigInteger.TWO.negate());
 
-    java.math.BigInteger e = (java.math.BigInteger.TWO.multiply(java.math.BigInteger.valueOf(3))).negative();
+    java.math.BigInteger e = (java.math.BigInteger.TWO.multiply(java.math.BigInteger.valueOf(3))).negate();
 
-    java.math.BigInteger f = java.math.BigInteger.TWO.negative().add(java.math.BigInteger.valueOf(9)).add(java.math.BigInteger.valueOf(12).multiply(java.math.BigInteger.valueOf(8))).subtract(java.math.BigInteger.valueOf(200).divide(java.math.BigInteger.valueOf(100)));
+    java.math.BigInteger f = java.math.BigInteger.TWO.negate().add(java.math.BigInteger.valueOf(9)).add(java.math.BigInteger.valueOf(12).multiply(java.math.BigInteger.valueOf(8))).subtract(java.math.BigInteger.valueOf(200).divide(java.math.BigInteger.valueOf(100)));
 
     public static void main(String[] args) {
-        java.math.BigInteger g = java.math.BigInteger.valueOf(4).negative();
+        java.math.BigInteger g = java.math.BigInteger.valueOf(4).negate();
         java.math.BigInteger k = java.math.BigInteger.TEN;
-        java.math.BigInteger l = java.math.BigInteger.valueOf(7).negative().add(java.math.BigInteger.TEN);
-        java.math.BigInteger m = java.math.BigInteger.valueOf(90).subtract(java.math.BigInteger.ONE.negative());
-        java.math.BigInteger n = (java.math.BigInteger.valueOf(21).multiply(java.math.BigInteger.valueOf(53))).negative();
-        java.math.BigInteger o = java.math.BigInteger.ONE.add((java.math.BigInteger.TWO.multiply(java.math.BigInteger.valueOf(3)))).subtract(java.math.BigInteger.valueOf(100).multiply((java.math.BigInteger.valueOf(12).subtract(java.math.BigInteger.valueOf(11))).negative())).subtract((java.math.BigInteger.valueOf(13).multiply((java.math.BigInteger.valueOf(12).divide(java.math.BigInteger.valueOf(3)))).add(java.math.BigInteger.ONE)).divide(java.math.BigInteger.valueOf(3)));
+        java.math.BigInteger l = java.math.BigInteger.valueOf(7).negate().add(java.math.BigInteger.TEN);
+        java.math.BigInteger m = java.math.BigInteger.valueOf(90).subtract(java.math.BigInteger.ONE.negate());
+        java.math.BigInteger n = (java.math.BigInteger.valueOf(21).multiply(java.math.BigInteger.valueOf(53))).negate();
+        java.math.BigInteger o = java.math.BigInteger.ONE.add((java.math.BigInteger.TWO.multiply(java.math.BigInteger.valueOf(3)))).subtract(java.math.BigInteger.valueOf(100).multiply((java.math.BigInteger.valueOf(12).subtract(java.math.BigInteger.valueOf(11))).negate())).subtract((java.math.BigInteger.valueOf(13).multiply((java.math.BigInteger.valueOf(12).divide(java.math.BigInteger.valueOf(3)))).add(java.math.BigInteger.ONE)).divide(java.math.BigInteger.valueOf(3)));
     }
 }
