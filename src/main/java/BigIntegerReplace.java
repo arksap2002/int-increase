b63d2b412698a570ee86f5a488c86df3a8f53998
Arkady <arksap2002@gmail.com>
2019-09-12 18:52:50 +0300

    Commit

--- src/main/java/BigIntegerReplace.java
+++ src/main/java/BigIntegerReplace.java
@@ -1,6 +1,7 @@
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.symbolsolver.JavaSymbolSolver;import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver;
+import com.github.javaparser.symbolsolver.JavaSymbolSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver;
 
 import static com.github.javaparser.ast.Node.SYMBOL_RESOLVER_KEY;
 
@@ -9,7 +10,7 @@ public final class BigIntegerReplace {
     public String transform(final String string) {
         CompilationUnit compilationUnit = JavaParser.parse(string);
         ReflectionTypeSolver reflectionTypeSolver =
-               new ReflectionTypeSolver();
+                new ReflectionTypeSolver();
         compilationUnit.setData(
                 SYMBOL_RESOLVER_KEY,
                 new JavaSymbolSolver(reflectionTypeSolver));
