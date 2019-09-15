f730de46545f4f98aab25dc266a404e36af4c9d4
Arkady <arksap2002@gmail.com>
2019-09-15 13:55:19 +0300

    Commit

--- src/test/java/BigIntegerReplaceTest.java
+++ src/test/java/BigIntegerReplaceTest.java
@@ -1,6 +1,7 @@
+import net.openhft.compiler.CompilerUtils;
 import org.apache.commons.io.IOUtils;
-import org.junit.Test;
 
+import org.junit.Test;
 import java.io.IOException;
 import java.nio.charset.Charset;
 
@@ -8,34 +9,43 @@ import static org.junit.Assert.*;
 
 public class BigIntegerReplaceTest {
 
-    private void runTestFromFile(String filePrefix) throws IOException {
+    private void runTestFromFile(String filePrefix) throws IOException, ClassNotFoundException {
         BigIntegerReplace bigIntegerReplace = new BigIntegerReplace();
-        String gotOutput = bigIntegerReplace.transform(IOUtils.resourceToString(filePrefix + ".before.java", Charset.defaultCharset()));
-        assertEquals(IOUtils.resourceToString(filePrefix + ".after.java", Charset.defaultCharset()), gotOutput);
+        CompilerUtils.CACHED_COMPILER.loadFromJava(filePrefix,
+                IOUtils.resourceToString("/" +
+                        filePrefix + ".before.java", Charset.defaultCharset()));
+        CompilerUtils.CACHED_COMPILER.loadFromJava(filePrefix,
+                IOUtils.resourceToString("/" +
+                        filePrefix + ".after.java", Charset.defaultCharset()));
+        String gotOutput = bigIntegerReplace.transform(
+                IOUtils.resourceToString("/" +
+                        filePrefix + ".before.java", Charset.defaultCharset()));
+        assertEquals(IOUtils.resourceToString("/" +
+                filePrefix + ".after.java", Charset.defaultCharset()), gotOutput);
     }
 
     @Test
-    public void testNothingChanges() throws IOException {
-        runTestFromFile("/NothingChanges");
-    }
-  
-    @Test
-    public void testReplaceVariableDeclarationType() throws IOException {
-        runTestFromFile("/ReplaceVariableDeclarationType");
+    public void testNothingChanges() throws IOException, ClassNotFoundException {
+        runTestFromFile("NothingChanges");
     }
 
     @Test
-    public void testScannerFromImport() throws IOException {
-        runTestFromFile("/ScannerFromImport");
+    public void testReplaceVariableDeclarationType() throws IOException, ClassNotFoundException {
+        runTestFromFile("ReplaceVariableDeclarationType");
     }
 
     @Test
-    public void testScannerWithClass() throws IOException {
-        runTestFromFile("/ScannerWithClass");
+    public void testScannerFromImport() throws IOException, ClassNotFoundException {
+        runTestFromFile("ScannerFromImport");
     }
 
     @Test
-    public void testToStringChanging() throws IOException {
-        runTestFromFile("/ToString");
+    public void testScannerWithClass() throws IOException, ClassNotFoundException {
+        runTestFromFile("ScannerWithClass");
+    }
+
+    @Test
+    public void testToStringChanging() throws IOException, ClassNotFoundException {
+        runTestFromFile("ToString");
     }
 }
