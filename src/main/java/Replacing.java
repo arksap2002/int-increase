1110fa684d842c07748cf4475161a8eb13b2c4bb
Arkady <arksap2002@gmail.com>
2019-09-26 14:11:49 +0300

    Commit

---
+++ src/main/java/Replacing.java
@@ -0,0 +1,143 @@
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.NodeList;
+import com.github.javaparser.ast.body.VariableDeclarator;
+import com.github.javaparser.ast.expr.Expression;
+import com.github.javaparser.ast.expr.MethodCallExpr;
+import com.github.javaparser.ast.expr.FieldAccessExpr;
+import com.github.javaparser.ast.expr.NameExpr;
+import com.github.javaparser.ast.expr.IntegerLiteralExpr;
+import com.github.javaparser.ast.expr.SimpleName;
+import com.github.javaparser.ast.expr.BinaryExpr;
+import com.github.javaparser.ast.expr.UnaryExpr;
+import com.github.javaparser.ast.type.ClassOrInterfaceType;
+import com.github.javaparser.ast.type.PrimitiveType;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+import com.github.javaparser.symbolsolver.javaparsermodel.JavaParserFacade;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver;
+
+import java.util.ArrayList;
+
+class Replacing {
+
+    private ArrayList<Runnable> changes = new ArrayList<>();
+
+    static void doReplace(final CompilationUnit compilationUnit,
+                          final ReflectionTypeSolver reflectionTypeSolver) {
+        Replacing replacing = new Replacing();
+        replacing.mainReplace(compilationUnit, reflectionTypeSolver);
+    }
+
+    private void mainReplace(final CompilationUnit compilationUnit,
+                             final ReflectionTypeSolver reflectionTypeSolver) {
+        compilationUnit.accept(new TransformVisitor(),
+                JavaParserFacade.get(reflectionTypeSolver));
+        for (Runnable change : changes) {
+            change.run();
+        }
+    }
+
+    private Expression createIntegerLiteralExpr(
+            final int number) {
+        FieldAccessExpr fieldAccessExpr = new FieldAccessExpr(
+                new FieldAccessExpr(
+                        new NameExpr("java"), "math"), "BigInteger");
+        if (number == 0) {
+            return new FieldAccessExpr(
+                    fieldAccessExpr, "ZERO");
+        } else if (number == 1) {
+            return new FieldAccessExpr(
+                    fieldAccessExpr, "ONE");
+        } else if (number == 2) {
+            return new FieldAccessExpr(
+                    fieldAccessExpr, "TWO");
+        } else if (number == /*CHECKSTYLE:OFF*/10/*CHECKSTYLE:ON*/) {
+            return new FieldAccessExpr(
+                    fieldAccessExpr, "TEN");
+        } else {
+            return new MethodCallExpr(
+                    fieldAccessExpr, "valueOf",
+                    new NodeList<>(new IntegerLiteralExpr(number)));
+        }
+    }
+
+    private String operationOfBinaryExpr(final BinaryExpr binaryExpr) {
+        if (binaryExpr.getOperator().equals(
+                BinaryExpr.Operator.PLUS)) {
+            return "add";
+        }
+        if (binaryExpr.getOperator().equals(
+                BinaryExpr.Operator.MINUS)) {
+            return "subtract";
+        }
+        if (binaryExpr.getOperator().equals(
+                BinaryExpr.Operator.MULTIPLY)) {
+            return "multiply";
+        }
+        if (binaryExpr.getOperator().equals(
+                BinaryExpr.Operator.DIVIDE)) {
+            return "divide";
+        }
+        if (binaryExpr.getOperator().equals(
+                BinaryExpr.Operator.REMAINDER)) {
+            return "remainder";
+        }
+        throw new UnsupportedOperationException();
+    }
+
+    private void makingAfter(final Expression n) {
+        if (n.isIntegerLiteralExpr()) {
+            changes.add(() -> n.replace(createIntegerLiteralExpr(
+                    n.asIntegerLiteralExpr().asInt())));
+        } else if (n.isMethodCallExpr()) {
+            if (n.asMethodCallExpr().resolve().getName().equals("nextInt")
+                    && n.asMethodCallExpr().resolve().getPackageName().
+                    equals("java.util") && n.asMethodCallExpr().resolve().
+                    getClassName().equals("Scanner")) {
+                if (n.asMethodCallExpr().getScope().isPresent()) {
+                    changes.add(() -> n.asMethodCallExpr().setName(
+                            new SimpleName("nextBigInteger")));
+                }
+            }
+        } else if (n.isBinaryExpr()) {
+            makingAfter(n.asBinaryExpr().getLeft());
+            makingAfter(n.asBinaryExpr().getRight());
+            n.replace(new MethodCallExpr(n.asBinaryExpr().getLeft(),
+                    operationOfBinaryExpr(n.asBinaryExpr()),
+                    new NodeList<>(n.asBinaryExpr().getRight())));
+        } else if (n.isEnclosedExpr()) {
+            makingAfter(n.asEnclosedExpr().getInner());
+        } else if (n.isUnaryExpr()) {
+            makingAfter(n.asUnaryExpr().getExpression());
+            if (n.asUnaryExpr().getOperator().equals(UnaryExpr.
+                    Operator.MINUS)) {
+                n.replace(new MethodCallExpr(
+                        n.asUnaryExpr().getExpression(), "negate"));
+            } else if (n.asUnaryExpr().getOperator().equals(UnaryExpr.
+                    Operator.PLUS)) {
+                n.replace(n.asUnaryExpr().getExpression());
+            } else {
+                throw new UnsupportedOperationException();
+            }
+        }
+    }
+
+        private class TransformVisitor
+                extends VoidVisitorAdapter<JavaParserFacade> {
+
+            @Override
+            public void visit(
+                    final VariableDeclarator n,
+                    final JavaParserFacade javaParserFacade) {
+                super.visit(n, javaParserFacade);
+                if (n.getType().equals(PrimitiveType.intType())) {
+                    if (n.getInitializer().isPresent()) {
+                        makingAfter(n.getInitializer().get());
+                    }
+                    changes.add(() -> n.setType(new ClassOrInterfaceType(
+                            new ClassOrInterfaceType(
+                                    new ClassOrInterfaceType("java"),
+                                    "math"), "BigInteger")));
+                }
+            }
+        }
+    }
