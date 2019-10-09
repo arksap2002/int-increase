0f191e062c1c1fff6a5d0dfbfacf88d5953a6d43
Arkady <arksap2002@gmail.com>
2019-10-09 20:36:16 +0300

    Commit

---
+++ src/test/resources/AssingExpr.before.java
@@ -0,0 +1,9 @@
+public class AssingExpr {
+
+    public static void main(String[] args) {
+        int a = 10;
+        a = 5;
+        a = ((1 + 2) * (100 - 12) / (-2) * (-1)) % 239;
+        a = Math.min(a, -18);
+    }
+}
