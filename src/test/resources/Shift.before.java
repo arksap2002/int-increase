52644f56a99c1320355a1a84492bfc580bd03755
Arkady <arksap2002@gmail.com>
2019-10-13 23:10:11 +0300

    Commit

---
+++ src/test/resources/Shift.before.java
@@ -0,0 +1,10 @@
+public class Shift {
+    public static void main(String[] args) {
+        int a = 1 >> 2;
+        int b = 2 << 10;
+        int f = 10 >> 2;
+        int c = 10;
+        int d = 2;
+        int e = d << c;
+    }
+}
