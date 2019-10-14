74469d9c813e74ed0b12f2213e27dada315c70c8
Arkady <arksap2002@gmail.com>
2019-10-14 20:45:01 +0300

    Commit

--- src/test/java/BigIntegerReplaceTest.java
+++ src/test/java/BigIntegerReplaceTest.java
@@ -84,4 +84,9 @@ public class BigIntegerReplaceTest {
     public void testBrief() throws IOException, ClassNotFoundException {
         runTestFromFile("Brief");
     }
+
+    @Test
+    public void testForWhile() throws IOException, ClassNotFoundException {
+        runTestFromFile("ForWhile");
+    }
 }
