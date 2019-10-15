16124e0a270e0b9747b069715f901a702cb5f417
Arkady <arksap2002@gmail.com>
2019-10-15 22:51:06 +0300

    Commit

--- src/test/resources/ForWhile.after.java
+++ src/test/resources/ForWhile.after.java
@@ -2,15 +2,15 @@ public class ForWhile {
 
     public static void main(String[] args) {
         for (java.math.BigInteger i = java.math.BigInteger.ZERO; i.compareTo(java.math.BigInteger.valueOf(200)) < 0; i = java.math.BigInteger.ONE.add(i)) {
-            System.out.println(-1);
+            System.out.println(java.math.BigInteger.ONE.negate());
         }
         java.math.BigInteger i = java.math.BigInteger.ZERO;
         while (i.compareTo(java.math.BigInteger.valueOf(100)) < 0) {
-            System.out.println(-1);
+            System.out.println(java.math.BigInteger.ONE.negate());
             i = java.math.BigInteger.ONE.add(i);
         }
         while (true) {
-            System.out.println(1);
+            System.out.println(java.math.BigInteger.ONE);
         }
     }
 }
