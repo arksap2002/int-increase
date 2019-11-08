9e015d63b8f12ff7901bfff02c9ad065a3b4f52b
Arkady <arksap2002@gmail.com>
2019-11-08 22:07:31 +0300

    Commit

--- src/test/resources/PartialReplacePrint.before.java
+++ src/test/resources/PartialReplacePrint.before.java
@@ -5,8 +5,9 @@ public class PartialReplacePrint {
     public static void main(String[] args) throws IOException {
         int /* BigInteger */ a = 2;
         int b = 9;
-        java.io.PrintWriter printWriter = new java.io.PrintWriter("output.out");
-        printWriter.print(a - b);
+        try (java.io.PrintWriter printWriter = new java.io.PrintWriter("output.out")) {
+            printWriter.print(a - b);
+        };
         System.out.println(a + b);
         Scanner scanner = new Scanner(System.in);
         b = scanner.nextInt();
