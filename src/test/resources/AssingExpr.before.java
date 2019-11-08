db54f91e15625c24e7798cf9d4b1a95e33e6da4e
Arkady <arksap2002@gmail.com>
2019-11-08 22:35:31 +0300

    Commit

--- src/test/resources/AssingExpr.before.java
+++ src/test/resources/AssingExpr.before.java
@@ -2,7 +2,6 @@ public class AssingExpr {
 
     public static void main(String[] args) {
         int /* BigInteger */ a = 10;
-        int /* BigInteger */ b = 10;
         int /* BigInteger */ c, d = 1;
         a = 5;
         a = ((1 + 2) * (100 - 12) / (-2) * (-1)) % 239;
