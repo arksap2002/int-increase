f96a880300d894fc8e7d51516ae06d3c49ee08b5
Arkady <arksap2002@gmail.com>
2019-12-09 22:48:35 +0300

    Commit

--- src/test/resources/Method.after.java
+++ src/test/resources/Method.after.java
@@ -7,9 +7,18 @@ public class Method {
         java.math.BigInteger[][] f = new java.math.BigInteger[10][a.intValue()];
         java.math.BigInteger d = foo(a.intValue(), new java.math.BigInteger(b), c);
         int e = foo(foo(foo(new java.math.BigInteger(c[0][0][0]).multiply(f[0][0]).intValue(), new java.math.BigInteger(b), c).add(java.math.BigInteger.TEN).subtract(a).intValue(), new java.math.BigInteger(b).add(a), c).intValue(), a.add(new java.math.BigInteger(b)), c).intValue();
+    // int /* BigInteger */ g = foo2(a, b, c);
+    // int h = foo(a, b, c);
     }
 
     private static java.math.BigInteger foo(int a, java.math.BigInteger b, int[][][] c) {
         return new java.math.BigInteger(a).add(b.multiply(new java.math.BigInteger(c[0][0][0]))).subtract(foo(new java.math.BigInteger(a).add(b).intValue(), new java.math.BigInteger(a).add(b), c));
     }
+    // private static int foo2(int a, int /* BigInteger */ b, int[][][] c) {
+    // if (a < 0) {
+    // return a + b * c[0][0][0] - foo(a + b, a + b, c) + foo2(a + b, a + b, c);
+    // } else {
+    // return a;
+    // }
+    // }
 }
--- src/test/resources/Method.before.java
+++ src/test/resources/Method.before.java
@@ -8,22 +8,18 @@ public class Method {
         int /* BigInteger */ d = foo(a, b, c);
         int e = foo(foo(foo(c[0][0][0] * f[0][0], b, c) + 10 - a, b + a, c), a + b, c);
 //        int /* BigInteger */ g = foo2(a, b, c);
-        int h = foo(a, b, c);
+//        int h = foo(a, b, c);
     }
 
     private static int /* BigInteger */ foo(int a, int /* BigInteger */ b, int[][][] c) {
-        if (a < 0) {
-            return a + b * c[0][0][0] - foo(a + b, a + b, c) + foo2(a + b, a + b, c);
-        } else {
-            return a;
-        }
+            return a + b * c[0][0][0] - foo(a + b, a + b, c);
     }
 
-    private static int foo2(int a, int /* BigInteger */ b, int[][][] c) {
-        if (a < 0) {
-            return a + b * c[0][0][0] - foo(a + b, a + b, c) + foo2(a + b, a + b, c);
-        } else {
-            return a;
-        }
-    }
+//    private static int foo2(int a, int /* BigInteger */ b, int[][][] c) {
+//        if (a < 0) {
+//            return a + b * c[0][0][0] - foo(a + b, a + b, c) + foo2(a + b, a + b, c);
+//        } else {
+//            return a;
+//        }
+//    }
 }
