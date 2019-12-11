f7caf832138c6ae96986e411aed2df0b12e49ea6
Arkady <arksap2002@gmail.com>
2019-12-11 22:20:05 +0300

    Commit

--- src/test/resources/Method.after.java
+++ src/test/resources/Method.after.java
@@ -7,23 +7,13 @@ public class Method {
         java.math.BigInteger[][] f = new java.math.BigInteger[10][a.intValue()];
         java.math.BigInteger d = foo(a.intValue(), java.math.BigInteger.valueOf(b), c);
         int e = foo(foo(foo(java.math.BigInteger.valueOf(c[0][0][0]).multiply(f[0][0]).intValue(), java.math.BigInteger.valueOf(b), c).add(java.math.BigInteger.TEN).subtract(a).intValue(), java.math.BigInteger.valueOf(b).add(a), c).intValue(), a.add(java.math.BigInteger.valueOf(b)), c).intValue();
-        java.math.BigInteger g = java.math.BigInteger.valueOf(foo2(a, b, c));
-        int h = foo2(a, b, c);
     }
 
     private static java.math.BigInteger foo(int a, java.math.BigInteger b, int[][][] c) {
         if (a < 0) {
-            return java.math.BigInteger.valueOf(a).add(b.multiply(java.math.BigInteger.valueOf(c[0][0][0]))).subtract(foo(java.math.BigInteger.valueOf(a).add(b).intValue(), java.math.BigInteger.valueOf(a).add(b), c)).add(java.math.BigInteger.valueOf(foo2(java.math.BigInteger.valueOf(a).add(b).intValue(), java.math.BigInteger.valueOf(a).add(b), c)));
+            return java.math.BigInteger.valueOf(a).add(b.multiply(java.math.BigInteger.valueOf(c[0][0][0]))).subtract(foo(java.math.BigInteger.valueOf(a).add(b).intValue(), java.math.BigInteger.valueOf(a).add(b), c));
         } else {
             return java.math.BigInteger.valueOf(a);
-        }
-    }
-
-    private static int foo2(int a, java.math.BigInteger b, int[][][] c) {
-        if (a < 0) {
-            return java.math.BigInteger.valueOf(a).add(b.multiply(java.math.BigInteger.valueOf(c[0][0][0]))).subtract(foo(java.math.BigInteger.valueOf(a).add(b).intValue(), java.math.BigInteger.valueOf(a).add(b), c)).add(java.math.BigInteger.valueOf(foo2(java.math.BigInteger.valueOf(a).add(b).intValue(), java.math.BigInteger.valueOf(a).add(b), c))).intValue();
-        } else {
-            return a;
         }
     }
 }
