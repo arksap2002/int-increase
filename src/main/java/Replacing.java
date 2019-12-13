18e9d01720618090d95bc81accf49739bcc516a2
Arkady <arksap2002@gmail.com>
2019-12-13 23:56:24 +0300

    Commit

--- src/main/java/Replacing.java
+++ src/main/java/Replacing.java
@@ -512,31 +512,19 @@ class Replacing {
             return isUpdateIntsToBitInt(n.asEnclosedExpr().getInner());
         }
         if (n.isUnaryExpr()) {
-            if (n.asUnaryExpr().getOperator().equals(
-                    UnaryExpr.Operator.POSTFIX_INCREMENT)) {
-                if ((n.asUnaryExpr().getExpression().isNameExpr()
-                        && isOfTypeInt(
-                        n.asUnaryExpr().getExpression().asNameExpr()))) {
-                    return isVariableToReplace(n.asUnaryExpr().
-                            getExpression().asNameExpr());
-                } else {
-                    return n.asUnaryExpr().getExpression().isArrayAccessExpr()
-                            && isVariableToReplace(getNameOfArray(
-                            n.asUnaryExpr().getExpression().
-                                    asArrayAccessExpr().getName()));
-                }
-            } else if (n.asUnaryExpr().getOperator().equals(
-                    UnaryExpr.Operator.POSTFIX_DECREMENT)) {
-                if ((n.asUnaryExpr().getExpression().isNameExpr()
-                        && isOfTypeInt(
-                        n.asUnaryExpr().getExpression().asNameExpr()))) {
-                    return isVariableToReplace(n.asUnaryExpr().
-                            getExpression().asNameExpr());
-                } else {
-                    return n.asUnaryExpr().getExpression().isArrayAccessExpr()
-                            && isVariableToReplace(getNameOfArray(
-                            n.asUnaryExpr().getExpression().
-                                    asArrayAccessExpr().getName()));
+            if ((n.asUnaryExpr().getExpression().isNameExpr()
+                && isOfTypeInt(
+                n.asUnaryExpr().getExpression().asNameExpr())) ||
+                (n.asUnaryExpr().getExpression().isArrayAccessExpr()
+                        && isVariableToReplace(getNameOfArray(
+                        n.asUnaryExpr().getExpression().asArrayAccessExpr().
+                                getName())))) {
+                if (n.asUnaryExpr().getOperator().equals(
+                        UnaryExpr.Operator.POSTFIX_INCREMENT)) {
+                    return true;
+                } else if (n.asUnaryExpr().getOperator().equals(
+                        UnaryExpr.Operator.POSTFIX_DECREMENT)) {
+                    return true;
                 }
             }
             return isUpdateIntsToBitInt(n.asUnaryExpr().getExpression());
